3cce9daa565acfdaac16c109cd9473387ef72c66
Lemon Fan <Lemon_Test@outlook.com>
2021-08-13 21:57:10 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class RWXml<T>
+    {
+        
+    }
+
+    public class XmlTemplateUtil<T>
+    {
+        public void Write(T obj, string filePaeh)
+        {
+            System.Xml.Serialization.XmlSerializer xml = new System.Xml.Serialization.XmlSerializer(typeof(T));
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Task> lis = new List<Task>();
+
+           // List<TaskFactory> tasks = new List<TaskFactory>();
+
+           TaskFactory taskFactory = new TaskFactory();
+
+            lis.Add(taskFactory.StartNew(()=> { }));
+            lis.Add(taskFactory.StartNew(() => { }));
+            lis.Add(taskFactory.StartNew(() => { }));
+            lis.Add(lis.ToArray(), t => { });
+
+        }
+    }
+}
